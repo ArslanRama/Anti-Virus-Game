e47e3e409ecfeaff870c33e7a468a18840a11fec
ArslanRama <arslanrama84@gmail.com>
2021-01-07 11:39:29 +0100

    set interval changed.

--- js/corona.js
+++ js/corona.js
@@ -2,13 +2,61 @@
 const container = document.querySelector("#container");
 const injection = document.querySelector("#injection");
 const pointsDiv = document.querySelector("#pointsDiv");
+const corona = document.querySelector('corona')
 
 // add mousemove event listener to control the injection position.
-
 container.addEventListener("mousemove", (e) => {
   injection.style.left = (e.clientX -12.5) + "px";
 });
 
+// create an array to save corona objects
+const coronaArr = []
+
+setInterval(()=>{
+  //get container width
+  let containerWidth = container.offsetWidth;
+  // create coorona div element
+  const coronaDiv = document.createElement('div');
+  // set class for corona div
+  coronaDiv.classList.add('corona')
+  // set the left position for the div randomly
+  let coronaLeft = Math.floor(Math.random()* containerWidth) + 1;
+  coronaDiv.style.left=coronaLeft+'px'
+
+  // add corona div to container
+  container.append(coronaDiv)
+
+  // create corona object
+  let coronaObj ={
+    coronaElement: coronaDiv,
+    top:0,
+    left:coronaLeft
+  }
+  // add the coronaObj to coronaArr
+  coronaArr.push(coronaObj)
+
+}, 1000)
+
+// create interval to move coronaDivs down by increasing the top property on the style
 setInterval(()=>{
 
-}, 500)
+  // get height of the container
+  let containerHeight = container.offsetHeight
+  
+  // loop through coronaArr to change the tip of coronaElements
+  coronaArr.forEach((element, index)=>{
+    // check the top of corona div is greater than the container's height so we need to delete the coronaDiv and coronaElement
+    if(element.top > containerHeight){
+      // delete coronaElement from html DOM
+      container.removeChild(element.coronaElement);
+      // delete the element from the array
+      coronaArr.splice(index, 1);
+    } else {
+      element.top += 10;
+      element.coronaElement.style.top = element.top + 'px';
+    }
+   
+  })
+
+}, 50)
+
