322c84861086f7190c32ef6bef9c685b07457fd4
ArslanRama <arslanrama84@gmail.com>
2021-01-07 14:31:00 +0100

    Added function to detect the virus and kill it.

--- js/corona.js
+++ js/corona.js
@@ -51,6 +51,9 @@ setInterval(()=>{
       container.removeChild(element.coronaElement);
       // delete the element from the array
       coronaArr.splice(index, 1);
+
+      // call explode function to detect if the bulletDiv touch the coronaDiv
+
     } else {
       element.top += 10;
       element.coronaElement.style.top = element.top + 'px';
@@ -59,6 +62,10 @@ setInterval(()=>{
   })
 
 }, 50)
+
+
+
+
 
 // create clik event listener for the container to create the bullet
 container.addEventListener("click", e => {
@@ -85,8 +92,42 @@ container.addEventListener("click", e => {
   } else {
     bottom += 25;
     bulletDiv.style.bottom = bottom + 'px';
+    // call explode function to detect if bulletDiv touches a coronaDiv
+    explode(bulletDiv, interval);
   }
   },50)
 
 })
 
+// explode function to detect if bulletDiv touch a coronaDiv
+function explode(bulletElement, interval){
+  // loop through coronaArr
+  coronaArr.forEach((corona, index)=>{
+    // check if coronaElement is in the same area as bulletElement
+    if(is_colliding(bulletElement, corona.coronaElement)){
+      clearInterval(interval);
+      container.removeChild(bulletElement);
+      coronaArr.splice(index, 1);
+      container.removeChild(corona.coronaElement);
+    } 
+     
+  })
+}
+
+let is_colliding = function( $div1, $div2 ) {
+  // Div 1 data
+  //var d1_offset             = $div1.offset();
+  let d1_height             = $div1.offsetHeight;
+  var d1_width              = $div1.offsetWidth;
+  let d1_distance_from_top  = $div1.offsettop + d1_height;
+  let d1_distance_from_left = $div1.offsetLeft + d1_width;
+  // Div 2 data
+  //var d2_offset             = $div2.offset();
+  let d2_height             = $div2.offsetHeight;
+  let d2_width              = $div2.offsetWidth;
+  let d2_distance_from_top  = $div2.offsetTop + d2_height;
+  let d2_distance_from_left = $div2.offsetLeft + d2_width;
+  let not_colliding = ( d1_distance_from_top < $div2.offsetTop || $div1.offsetTop > d2_distance_from_top || d1_distance_from_left < $div2.offsetLeft || $div1.offsetLeft > d2_distance_from_left );
+  // Return whether it IS colliding
+  return ! not_colliding;
+};
